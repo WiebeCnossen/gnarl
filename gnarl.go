80ca84c61098c495205dc8608947f324de161a25
Wiebe Cnossen <wiebe.cnossen@gmail.com>
2021-11-17 21:26:43 +0100

    audit and check verbs

--- gnarl.go
+++ gnarl.go
@@ -36,14 +36,17 @@ func mustSaveLock(lock *yarn.Lock) bool {
 	return dirty
 }
 
-const version string = "1.0.0-beta-9"
+const version string = "1.0.0-rc-1"
 
 func help() {
 	log.Printf("gnarl %s - the yarn v2/v3 companion tool", version)
-	log.Print("Usage: gnarl [<auto | fix | help | reset> <args>]")
-	log.Print("> gnarl [auto] [reset-package-names...]")
+	log.Print("usage: gnarl [<auto | audit | check | fix | help | reset> <args>]")
+	log.Print("> gnarl [auto]")
+	log.Print("> gnarl audit")
+	log.Print("> gnarl check")
 	log.Print("> gnarl fix package-name safe-version-request")
 	log.Print("> gnarl help")
+	log.Print("> gnarl shrink")
 	log.Print("> gnarl reset package-names...")
 }
 
@@ -51,23 +54,24 @@ func main() {
 	var verb string
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
-		case "deprecated-shrink":
+		case "audit":
+		case "check":
 		case "fix":
 		case "help":
 		case "reset":
-			verb = os.Args[1]
+		case "shrink":
 		default:
-			verb = "auto"
+			log.Fatalf("unknown verb: %s", os.Args[1])
 		}
+		verb = os.Args[1]
 	} else {
 		verb = "auto"
 	}
 
-	var lock *yarn.Lock
 	project := mustReadPackage()
+
 	switch verb {
 	case "auto":
-		first := true
 		for {
 			log.Print("yarn install")
 			_, err := exec.Command("yarn", "install").Output()
@@ -81,87 +85,111 @@ func main() {
 				log.Fatal(err)
 			}
 
-			log.Print("yarn npm audit --recursive")
-			out, err := exec.Command("yarn", "npm", "audit", "--json", "--recursive").Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			advisories, err := yarn.ParseAudit(out)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			lock = mustReadLock()
-
-			if first {
-				first = false
-				for _, arg := range os.Args {
-					lock.Reset(arg)
-				}
-			}
-
-			for _, advisory := range advisories {
-				request, err := semver.ParseRequest(advisory.PatchedVersions)
-				if err != nil {
-					log.Fatalf("Invalid safe-version-request: %v", err)
-				}
-
-				lock.Fix(advisory.ModuleName, request)
-			}
-
-			for key, r := range project.Resolutions {
-				parts := strings.Split(key, "@")
-				npmPackage := parts[0]
-				request := "*"
-				switch len(parts) {
-				case 1:
-					if v, err := semver.ParseRequest(r); err == nil && !v.IsExact() {
-						log.Printf("Unrestricted resolution for %s", npmPackage)
-					}
-				default:
-					request = parts[1]
-				}
-
-				if !lock.Has(npmPackage, request) {
-					log.Printf("Superfluous resolution for %s", key)
-				}
-			}
-
-			if !mustSaveLock(lock) {
+			if !audit(project) {
 				break
 			}
 		}
-	case "deprecated-shrink":
-		lock = mustReadLock()
-		lock.Shrink()
-		mustSaveLock(lock)
+
+	case "audit":
+		audit(project)
+
+	case "check":
+		lock := mustReadLock()
+		check(project, lock)
+
 	case "fix":
 		if len(os.Args) < 4 {
 			help()
-			log.Fatal("Insufficient arguments")
+			log.Fatal("insufficient arguments")
 		}
 
 		npmPackage := os.Args[2]
 		request, err := semver.ParseRequest(strings.Join(os.Args[3:], " "))
 		if err != nil {
-			log.Fatalf("Invalid safe-version-request: %v", err)
+			log.Fatalf("invalid safe-version-request: %v", err)
 		}
 
-		lock = mustReadLock()
+		lock := mustReadLock()
 		lock.Fix(npmPackage, request)
 		mustSaveLock(lock)
+
 	case "help":
 		help()
+
 	case "reset":
-		lock = mustReadLock()
+		lock := mustReadLock()
 
 		for _, arg := range os.Args[1:] {
 			lock.Reset(arg)
 		}
 
 		mustSaveLock(lock)
+
+	case "shrink":
+		lock := mustReadLock()
+		lock.Shrink()
+		mustSaveLock(lock)
+
 	default:
-		log.Fatalf("Unreachable verb %s", verb)
+		log.Fatalf("unreachable verb %s", verb)
+	}
+}
+
+func audit(project *yarn.Package) bool {
+	log.Print("yarn npm audit --recursive")
+	out, err := exec.Command("yarn", "npm", "audit", "--json", "--recursive").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	advisories, err := yarn.ParseAudit(out)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lock := mustReadLock()
+
+	for _, advisory := range advisories {
+		request, err := semver.ParseRequest(advisory.PatchedVersions)
+		if err != nil {
+			log.Fatalf("invalid safe-version-request: %v", err)
+		}
+
+		lock.Fix(advisory.ModuleName, request)
+	}
+
+	if len(advisories) == 0 {
+		log.Print("all packages considered safe")
+	}
+
+	check(project, lock)
+
+	return mustSaveLock(lock)
+}
+
+func check(project *yarn.Package, lock *yarn.Lock) {
+	dirty := false
+	for key, r := range project.Resolutions {
+		parts := strings.Split(key, "@")
+		npmPackage := parts[0]
+		request := "*"
+		switch len(parts) {
+		case 1:
+			if v, err := semver.ParseRequest(r); err == nil && !v.IsExact() {
+				dirty = true
+				log.Printf("unrestricted resolution for %s", npmPackage)
+			}
+		default:
+			request = parts[1]
+		}
+
+		if !lock.Has(npmPackage, request) {
+			dirty = true
+			log.Printf("superfluous resolution for %s", key)
+		}
+	}
+
+	if !dirty {
+		log.Print("all resolutions good")
 	}
 }
